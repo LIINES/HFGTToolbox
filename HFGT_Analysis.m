c114c8593c645df68af3ef843035c956fa994b14
Dakota Thompson <Dakota.J.Thompson.TH@Dartmouth.edu>
2020-08-28 12:25:21 -0400

    Added Petri net GUI functionality and dependency on tensors

--- HFGT_Analysis.m
+++ HFGT_Analysis.m
@@ -1,13 +1,21 @@
 close all
 clear all
 clc 
-addpath(genpath('raw2FullLFES'));
-addpath(genpath('XML2LFES'));
+addpath('1-XML2LFES/')
+addpath('2-Raw2FullLFES/')
+addpath('3-PetriNets/')
+% ensure teh tensor_toolbox is in the working directory search path
+% 'https://www.tensortoolbox.org'
 
 XMLFile='Example_Network.xml';
-% XMLFile='Trimetrica-3layer-Data-v9.xml';
 %% Section:  Construct myLFES "raw" Structure
 [myLFES,S]=XML2LFES(XMLFile)
 
 %% Section:  Make myLFES "full"
 myLFES=raw2FullLFES(myLFES)
+
+%% Section: Call the HFGT GUI
+% XMLFILE='Example_Network_PN.xml';
+% EventList='exampleEventList_PN.csv';
+
+HFGT_GUI
