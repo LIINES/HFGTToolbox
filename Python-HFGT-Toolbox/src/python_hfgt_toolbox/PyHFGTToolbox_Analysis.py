3ccc9e0d1e7e444fd68ed927934b7e4f8ae6819e
Dakota Thompson <dakota.j.thompson.th@dartmouth.edu>
2023-05-03 17:27:30 -0400

    Added functionality to save look-up tables of operands, buffers, and DOFs for the first three verbose modes.  These are saved in the 'Data/LookUpTable/' folder.

--- Python-HFGT-Toolbox/src/python_hfgt_toolbox/PyHFGTToolbox_Analysis.py
+++ Python-HFGT-Toolbox/src/python_hfgt_toolbox/PyHFGTToolbox_Analysis.py
@@ -35,16 +35,43 @@ def run_analysis(xml_file: str, verbose_mode: int, out_dir = 'output'):
     with open(output_pkl, 'wb') as afile:
         pickle.dump(myLFES, afile)
 
-    save_dir = Path("data/IncidenceMatrices")
+    save_dir = Path("data/")
     save_dir.mkdir(parents=True, exist_ok=True)
     if myLFES.MRT != 0:
-        np.savetxt(f"{save_dir}/MRT.csv", np.transpose(np.append(myLFES.MRT.coords,[myLFES.MRT.data], axis=0)), delimiter=",", header="Operand,Buffer,DOF,Value",comments='')
-    np.savetxt(f"{save_dir}/MRT_neg.csv", np.transpose(np.append(myLFES.MRT_neg.coords,[myLFES.MRT_neg.data], axis=0)), delimiter=",", header="Operand,Buffer,DOF,Value",comments='')
-    np.savetxt(f"{save_dir}/MRT_pos.csv", np.transpose(np.append(myLFES.MRT_pos.coords,[myLFES.MRT_pos.data], axis=0)), delimiter=",", header="Operand,Buffer,DOF,Value",comments='')
+        np.savetxt(f"{save_dir}/IncidenceMatrices/MRT.csv", np.transpose(np.append(myLFES.MRT.coords,[myLFES.MRT.data], axis=0)), delimiter=",", header="Operand,Buffer,DOF,Value",comments='')
+    np.savetxt(f"{save_dir}/IncidenceMatrices/MRT_neg.csv", np.transpose(np.append(myLFES.MRT_neg.coords,[myLFES.MRT_neg.data], axis=0)), delimiter=",", header="Operand,Buffer,DOF,Value",comments='')
+    np.savetxt(f"{save_dir}/IncidenceMatrices/MRT_pos.csv", np.transpose(np.append(myLFES.MRT_pos.coords,[myLFES.MRT_pos.data], axis=0)), delimiter=",", header="Operand,Buffer,DOF,Value",comments='')
     if myLFES.MRTproj != 0:
-        np.savetxt(f"{save_dir}/MRTproj.csv", np.transpose(np.append(myLFES.MRTproj.coords,[myLFES.MRTproj.data], axis=0)), delimiter=",", header="Operand,Buffer,DOF,Value",comments='')
-        np.savetxt(f"{save_dir}/MRTproj_neg.csv", np.transpose(np.append(myLFES.MRTproj_neg.coords,[myLFES.MRTproj_neg.data], axis=0)), delimiter=",", header="Operand,Buffer,DOF,Value",comments='')
-        np.savetxt(f"{save_dir}/MRTproj_pos.csv", np.transpose(np.append(myLFES.MRTproj_pos.coords,[myLFES.MRTproj_pos.data], axis=0)), delimiter=",", header="Operand,Buffer,DOF,Value",comments='')
+        np.savetxt(f"{save_dir}/IncidenceMatrices/MRTproj.csv", np.transpose(np.append(myLFES.MRTproj.coords,[myLFES.MRTproj.data], axis=0)), delimiter=",", header="Operand,Buffer,DOF,Value",comments='')
+        np.savetxt(f"{save_dir}/IncidenceMatrices/MRTproj_neg.csv", np.transpose(np.append(myLFES.MRTproj_neg.coords,[myLFES.MRTproj_neg.data], axis=0)), delimiter=",", header="Operand,Buffer,DOF,Value",comments='')
+        np.savetxt(f"{save_dir}/IncidenceMatrices/MRTproj_pos.csv", np.transpose(np.append(myLFES.MRTproj_pos.coords,[myLFES.MRTproj_pos.data], axis=0)), delimiter=",", header="Operand,Buffer,DOF,Value",comments='')
+
+    if verbose_mode < 3:
+        np.savetxt(f"{save_dir}/LookUpTable/Operands.csv",np.transpose([range(myLFES.numOperands), myLFES.setOperands]), delimiter=",", header="Index,Operands",comments='', fmt="%s")
+        np.savetxt(f"{save_dir}/LookUpTable/Buffers.csv",np.transpose([range(myLFES.numBuffers), np.append(myLFES.machines.name,myLFES.indBuffers.name)]), delimiter=",", header="Index,Operands",comments='', fmt="%s")
+
+        idxAS = np.nonzero(myLFES.AS.T)
+        DOFs = []
+        idxDOFs = []
+        idxDOFsProj = []
+        for k1 in range(len(idxAS[0])):
+            if idxAS[0][k1] < myLFES.numMachines:
+                resource = myLFES.machines.name[idxAS[0][k1]]
+            elif idxAS[0][k1] < myLFES.numBuffers:
+                resource = myLFES.indBuffers.name[idxAS[0][k1]-myLFES.numMachines]
+            else:
+                resource = myLFES.transporters.name[idxAS[0][k1]-myLFES.numBuffers]
+
+            if idxAS[1][k1] < myLFES.numTransformProcess:
+                process = myLFES.setTransformProcess[idxAS[1][k1]]
+            else:
+                process = myLFES.setTransportRefProcess[idxAS[1][k1]-myLFES.numTransformProcess]
+
+            DOFs.append(resource + ': ' + process)
+            idxDOFs.append(idxAS[0][k1] * myLFES.numProcesses + idxAS[1][k1])
+            idxDOFsProj.append(k1)
+
+        np.savetxt(f"{save_dir}/LookUpTable/DOFs.csv",np.transpose([idxDOFsProj, idxDOFs, DOFs]), delimiter=",", header="ProjectedIndex,Index,DOF",comments='', fmt="%s")
 
     return output_pkl.absolute()
 
